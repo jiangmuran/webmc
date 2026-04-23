23dcdb4b528ad7b3c9a06ddf5c3902f13332b3f5
Andy Jiang <72869031+jiangmuran@users.noreply.github.com>
2026-04-23 18:13:28 +0800

    Fix: prettier container_row_split

--- src/ui/container_row_split.ts
+++ src/ui/container_row_split.ts
@@ -14,5 +14,8 @@ export function splitStackInHalf(s: Stack): [Stack | undefined, Stack | undefine
 
 export function takeOneFromStack(s: Stack): [Stack | undefined, Stack] {
   if (s.count <= 1) return [undefined, { id: s.id, count: 1 }];
-  return [{ id: s.id, count: s.count - 1 }, { id: s.id, count: 1 }];
+  return [
+    { id: s.id, count: s.count - 1 },
+    { id: s.id, count: 1 },
+  ];
 }
